b3546d6d7bdb0b68a47b4ff6194278eafc6cb3a0
Federico Chiariotti <chiariot@dei.unipd.it>
2025-06-09 09:37:11 +0200

    Local anomaly scripts

---
+++ local_scheduler.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+
+class LocalAnomalyScheduler:
+    N = 0
+    psi = []
+    lambdas = []
+    debug_mode = False
+
+    def __init__(this, N, activation, debug_mode):
+        this.N = N
+        this.psi = np.ones(N)
+        this.lambdas = np.ones(N) * activation
+        this.debug_mode = debug_mode
+
+    def schedule(this, P, Q, p_thr):
+        if (this.debug_mode):
+            print('p', this.psi)
+        for threshold in np.arange(0, np.max(this.psi) - 1, 1):
+            act_prob = np.zeros(this.N)
+            for n in range(this.N):
+                act_prob[n] = 1 - np.power(1 - this.lambdas[n], np.max([0, this.psi[n] - threshold]))
+            A = np.sum(act_prob > 0)
+            ## TODO separate nodes that are too high
+            activation = np.mean(act_prob)
+            coll = 0
+            for a in np.arange(2, A + 1, 1):
+                p_a = np.power(activation, a) * np.power(1 - activation, A - a) * math.comb(A, a)
+                if (a < P):
+                    p_c = 1 - math.factorial(P) / math.factorial(P - a) / np.power(P, a)
+                else:
+                    p_c = 1
+                coll += p_a * p_c
+            if (this.debug_mode):
+                print('z',threshold,coll)
+            if (coll < p_thr):
+                return threshold
+        return np.max([0, np.max(this.psi) - 2])
+
+    def update_psi(this, threshold, outcome):
+        for i in range(this.N):
+            # Node i successfully transmitted, we reset AoII to 0
+            if (i in outcome):
+                this.psi[i] = 0
+        # There are no collisions
+        if (np.min(outcome) > -0.1):
+            # No remaining node has an AoII higher than the threshold
+            this.psi = np.minimum(this.psi, np.ones(this.N) * threshold)
+        # Next time step
+        this.psi += 1
---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import local_scheduler as l_sch
+
+## Main system parameters ##
+nodes = 100
+activation = 0.04
+P = 10
+Q = 10
+T = int(1e5)
+p_c = 1 / 3
+debug_mode = False
+
+## Utility variables ##
+state = np.zeros(nodes)
+aoii = np.zeros((T, nodes))
+
+local_sched = l_sch.LocalAnomalyScheduler(nodes, activation, debug_mode)
+
+for t in range(T):
+    # Generate anomalies
+    state = np.minimum(np.ones(nodes), state + np.asarray(np.random.rand(nodes) < activation))
+    if (t > 0):
+        aoii[t, :] = aoii[t - 1, :] + state
+    else:
+        aoii[t, :] = state
+    ## TODO collision resolution?
+    threshold = local_sched.schedule(P, Q, p_c)
+    # Select random slots for active nodes
+    choices = np.random.randint(1, P + 1, nodes) * np.asarray(aoii[t, :] > threshold) - 1
+    outcome = np.zeros(P)
+    for p in range(P):
+        chosen = np.where(choices == p)[0]
+        if (chosen.size != 0):
+            if (chosen.size == 1):
+                outcome[p] = chosen[0]
+                state[chosen[0]] = 0
+                aoii[t, chosen[0]] = 0
+            else:
+                outcome[p] = -1
+    local_sched.update_psi(threshold, outcome)
+    if(np.mod(t,1000) == 0):
+        print('Step:', t)
+    if (debug_mode):
+        print('s', state)
+        print('t', threshold)
+        print('c', choices)
+        print('o', outcome)
+        print('a',aoii[t,:])
+        input("Press Enter to continue...")
+
+
+aoii_tot = np.reshape(aoii, T * nodes)
+values = np.arange(0, 101, 1)
+plt.hist(aoii_tot, values - 0.5, density = True)
+plt.show()
