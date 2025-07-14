86a51b26e40335e13a0ec407ce66d98cddc63fd1
lostinafro <saggese.fabio@gmail.com>
2025-07-14 14:59:31 +0200

    Added common file for functions

--- common.py
+++ common.py
@@ -9,6 +9,7 @@ D = 20
 
 # Frame
 max_num_frame = int(1e5)
+frame_duration = 10e-3      # 10 ms
 S = 25
 S_step = 5
 Q_vec = np.arange(5, S + S_step, S_step, dtype=int)
@@ -25,3 +26,12 @@ risk_thr_vec = np.arange(0., 1. + risk_step, risk_step, dtype=float)
 
 def std_bar(total_iteration):
     return tqdm(total_iteration, file=sys.stdout, leave=False, ncols=60, ascii=True)
+
+
+# NumPy
+def compute_ecdf(metric):
+    sorted_vals = np.sort(metric)
+    cdf_vals = np.linspace(0, 1, metric.shape[0])
+    return sorted_vals, cdf_vals
+
+
