d89212667041f8b7bf617bc73f422a5a49e32df3
lostinafro <saggese.fabio@gmail.com>
2025-07-18 11:23:42 +0200

    Added support for heterogeneous and homogeneous clusters

--- dist_rate_test.py
+++ dist_rate_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from common import C, frame_duration, std_bar
+from common import C, frame_duration, p_11
 
 def index_to_cluster_state(idx, cluster_size) -> np.ndarray:
     """Translator from an index in the pmf to a cluster state.
@@ -67,52 +67,56 @@ def get_absorption_time(transition_matrix, cluster_size):
 def nodes_ratio(p_vec):
     return np.tile(p_vec, (len(p_vec), 1)) / np.tile(p_vec, (len(p_vec), 1)).T
 
+
+def compute_absorption_rate(p_01_vec: np.ndarray, p_11: float,  multiplier: np.ndarray, show :bool = True):
+    """ Compute the absorption rate given the initial transition probabilities with different multipliers.
+    Needed to find the actual multipliers used for running the tests.
+
+    :param p_01_vec:
+    :param p_11:
+    :param multiplier:
+    :param show:
+    :return:
+    """
+    absorption_rate = np.zeros(len(multiplier))
+    cluster_size = len(p_01_vec)
+    # Start the loop for different loads
+    for i, mult in enumerate(multiplier):
+        # Different clusters
+        p_01 = mult * p_01_vec
+        transition_matrix = init_transition_matrix(cluster_size, p_01, p_11)
+        T = get_absorption_time(transition_matrix, cluster_size)
+        absorption_rate[i] = 1 / T[0] / frame_duration
+        if show:
+            print(f'p_01: {np.round(p_01, 5)}')
+            print(f'\tAbsorption rate: {absorption_rate[i]:0.3f}')
+    return absorption_rate
+
 ### MAIN SCRIPT ###
 if __name__ == '__main__':
-    cluster_size = C
-    p_11 = 0.9
-
-    # p_01_base = np.array([0.001, 0.00704, 0.00725, 0.00750])    # gives 0.25
-    # p_01_diff = np.array([0.002332, 0.01642, 0.01691, 0.01749])
 
     ### QUASI HETEROGENEOUS ###
-    p_01_quasi_het = np.array([0.002332, 0.01642, 0.01691, 0.01749])
-    multiplier = np.array([1., 1.5984, 2.1425, 2.663, 3.17])
+    qhet_p_01 = np.array([0.002332, 0.01642, 0.01691, 0.01749])
+    qhet_mult_int = np.array([1., 1.5984, 2.1425, 2.663, 3.17])
+    qhet_multipliers = np.linspace(qhet_mult_int[0], qhet_mult_int[-1], 40)  # useful for load test
     print('Quasi-heterogeneous clusters:')
-    # quasi_het_ratio = nodes_ratio(p_01_quasi_het)
-    # print('ratio', quasi_het_ratio, '\n')
-    for mult in multiplier:
-        # Different clusters
-        p_01 = mult * p_01_quasi_het
-        print(f'p_01: {np.round(p_01, 5)}')
-        transition_matrix = init_transition_matrix(cluster_size, p_01, p_11)
-        T = get_absorption_time(transition_matrix, cluster_size)
-        print(f'\tAbsorption rate: {1/T[0] / frame_duration:0.3f}')
+    qhet_ratio = nodes_ratio(qhet_p_01)
+    print('ratio\n', qhet_ratio, '\n')
+    qhet_absorption_rate = compute_absorption_rate(qhet_p_01, p_11, qhet_multipliers)
+
 
     ### HETEROGENEOUS ###
-    p_01_het = np.array([0.001, 0.005, 0.009, 0.013])
-    het_multiplier = np.array([1.974, 3.162, 4.245, 5.282, 6.292])
-
+    het_p_01 = np.array([0.001, 0.005, 0.009, 0.013])
+    het_multipliers = np.array([1.974, 3.162, 4.245, 5.282, 6.292])
     print('\nHeterogeneous clusters:')
-    het_ratio = nodes_ratio(p_01_het)
-    print('ratio', het_ratio, '\n')
-    for mult in het_multiplier:
-        # Different clusters
-        p_01 = mult * p_01_het
-        print(f'p_01: {np.round(p_01, 5)}')
-        transition_matrix = init_transition_matrix(cluster_size, p_01, p_11)
-        T = get_absorption_time(transition_matrix, cluster_size)
-        print(f'\tAbsorption rate: {1 / T[0] / frame_duration:0.3f}')
+    het_ratio = nodes_ratio(het_p_01)
+    print('ratio\n', het_ratio, '\n')
+    het_absorption_rate = compute_absorption_rate(het_p_01, p_11, het_multipliers)
 
     ### HOMOGENEOUS ###
-    p_01_homo = p_01_het.mean() * np.ones(cluster_size)
-    homo_multiplier = np.array([1.82, 2.909, 3.899, 4.847, 5.771])
-
-    print('\nHeterogeneous clusters:')
-    for mult in homo_multiplier:
-        # Different clusters
-        p_01 = mult * p_01_homo
-        print(f'p_01: {np.round(p_01, 5)}')
-        transition_matrix = init_transition_matrix(cluster_size, p_01, p_11)
-        T = get_absorption_time(transition_matrix, cluster_size)
-        print(f'\tAbsorption rate: {1 / T[0] / frame_duration:0.3f}')
+    homo_p_01 = het_p_01.mean() * np.ones(len(het_p_01))
+    homo_multipliers = np.array([1.82, 2.909, 3.899, 4.847, 5.771])
+    print('\nHomogeneous clusters:')
+    homo_ratio = nodes_ratio(homo_p_01)
+    print('ratio\n', homo_ratio, '\n')
+    homo_absorption_rate = compute_absorption_rate(homo_p_01, p_11, homo_multipliers)
