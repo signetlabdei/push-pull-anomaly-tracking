31017161b48f45fd93ef57c6b7a1dfde985ba50a
lostinafro <saggese.fabio@gmail.com>
2025-07-18 15:26:17 +0200

    Edited presentation, added distributed anomaly tracking

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import local_scheduler as l_sch
-import distributed_scheduler as d_sch
-import push_pull_manager as pp_man
-from common import Q_vec, risk_thr_vec, C, D, p_01_qhet, multiplier, p_11, distributed_detection, std_bar, pull_folder
+from local_scheduler import LocalAnomalyScheduler
+from distributed_scheduler import DistributedAnomalyScheduler
+from push_pull_manager import ResourceManager
+from common import Q_vec, risk_thr_vec, C, D, qhet_p_01, qhet_multipliers, p_11, distributed_detection, std_bar, pull_folder
 
 # Main system parameters
 nodes = 100
@@ -12,14 +12,15 @@ M = 100     # S = 20
 R = 20
 T = int(1e5)
 mode = 1
-debug_mode = False
+debug_mode = True
 
 # Anomaly parameters
 local_anomaly_rate = 0.03
 distributed_cluster_size = 4
 distributed_cluster_number = 10
-p_01 = p_01_qhet * multiplier[2]
+p_01 = qhet_p_01 * qhet_multipliers[2]
 p_11 = 0.9
+risk_thr = 0.5
 
 # Algorithm parameters
 p_c = 0.2
@@ -32,18 +33,19 @@ rng = np.random.default_rng(0)
 clustered = distributed_cluster_number * distributed_cluster_size   # The first clustered nodes have distributed anomalies
 local_state = np.zeros(nodes)
 distributed_state = np.zeros(clustered)
+distributed_anomaly = np.zeros(distributed_cluster_number)
 aoii = np.zeros((T, nodes))
 scheduled = []
 
 # Instantiate schedulers
-local_sched = l_sch.LocalAnomalyScheduler(nodes, max_age, local_anomaly_rate, mode, debug_mode)
-dist_sched = d_sch.DistributedAnomalyScheduler(clustered, distributed_cluster_size, p_01, p_11, debug_mode)
-manager = pp_man.ResourceManager(manager_type, R)
+local_sched = LocalAnomalyScheduler(nodes, max_age, local_anomaly_rate, mode, debug_mode)
+dist_sched = DistributedAnomalyScheduler(clustered, distributed_cluster_size, p_01, p_11, debug_mode)
+manager = ResourceManager(manager_type, R)
 manager.set_push_resources(10) # Set P (only used by manager 0)
 manager.set_min_threshold(5) # Set minimum resources for each subframe (used by managers 1 and 2, ensures push doesn't go below 5)
 manager.set_hysteresis(0.005) # Set hysteresis threshold (only used by manager 2, it won't change resource allocation if the difference between the two risks is smaller than the threshold)
 
-for t in range(T):
+for t in std_bar(range(T)):
     ### ANOMALY GENERATION ###
     local_state = np.minimum(np.ones(nodes), local_state + np.asarray(np.random.rand(nodes) < local_anomaly_rate))
     new_state = np.zeros(clustered)
@@ -59,27 +61,32 @@ for t in range(T):
                 p = p_11
         new_state[node] = rng.random() < p
     distributed_state = new_state
+
+    # Compute distributed anomaly z^{(i)}(k)
+    for cluster in range(distributed_cluster_number):
+        distributed_anomaly[cluster] = np.sum(distributed_state[dist_sched.cluster_map == cluster]) >= cluster_size / 2
+
+    ### COMPUTE AOII ###
     if t > 0:
         aoii[t, :] = aoii[t - 1, :] + local_state
     else:
         aoii[t, :] = local_state
 
-
     ### UPDATE SCHEDULER PRIORS ###
     local_sched.update_prior()
     dist_sched.update_prior()
-    # TODO add this at the beginning of each frame in other scripts
 
     ### SUBFRAME ALLOCATION ###
     local_risk = local_sched.get_risk(aoii_threshold)
     dist_risk = dist_sched.get_average_risk
     P, Q = manager.allocate_resources(local_risk, dist_risk) # Allocate resources
-    print(local_risk,dist_risk,local_risk/dist_risk,P,Q)
+    if debug_mode:
+        print('local_risk', local_risk, 'dist_risk', dist_risk, 'ratio', local_risk/dist_risk)
+        print('P', P, 'Q', Q)
 
     ### PULL-BASED SUBFRAME ###
     # Get pull schedule
-    scheduled = dist_sched.schedule(Q, 0.3)
-    # print('s', scheduled)
+    scheduled = dist_sched.schedule(Q, risk_thr)
     # Fix local anomalies in scheduled slots
     aoii[t, scheduled] = 0
     local_state[scheduled] = 0
@@ -111,16 +118,8 @@ for t in range(T):
     # Reset state for cluster where an anomaly was found
     for cluster in cluster_in_anomaly:
         distributed_state[distributed_cluster_size * (cluster - 1): distributed_cluster_size * cluster] = 0
-    # OLD DEPRECATED
-    # Reset detected distributed anomalies
-    # for c in range(distributed_cluster_number):
-    #     anomaly = dist_sched.get_cluster_risk(c)
-    #     if (anomaly >= distributed_detection):
-    #         distributed_state[distributed_cluster_size * (c - 1) : distributed_cluster_size * c] = 0
 
     ### LOGGING ###
-    if np.mod(t,1000) == 0:
-        print('Step:', t)
     if debug_mode:
         print('s', local_state)
         print('t', threshold)
