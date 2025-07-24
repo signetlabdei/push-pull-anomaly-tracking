3e3b58790a0baf56e334850c2750d884db4812d1
lostinafro <saggese.fabio@gmail.com>
2025-07-24 14:56:14 +0200

    Added many common variables

--- common.py
+++ common.py
@@ -11,47 +11,45 @@ pull_folder = os.path.join(data_folder, 'pull_only')
 coexistence_folder = os.path.join(data_folder, 'coexistence')
 
 # Main system parameters
-N = 100
-C = 4
-D = 20
+M = 100             # Num bins histogram
+N = 100             # Num nodes N_a
+C = 4               # cluster size
+D = 20              # Num clusters
+T = int(1e3)        # Num frames
+E = 100             # num episodes
+RNG = np.random.default_rng(0)  # Random Number Generator
 
-# Frame
-max_num_frame = int(1e5)
+# Resource grid
 frame_duration = 10e-3      # 10 ms
 R = 20
 R_step = 1
 Q_vec = np.arange(5, R + R_step, R_step, dtype=int)
 P_vec = np.arange(5, R + R_step, R_step, dtype=int)
 
-# Local parameters
+# Anomaly parameters
 max_age = 100
+SIGMA = 0.2
 
 ### MISALIGNMENT PARAMETERS ###
-p_11 = 0.9
-
-# Basic load
-abs_rate_int = np.array([1., 2., 3., 4., 5.])    # anomalies/node/s related to the multiplier
+p11 = 0.9
 
 # Load parameters for quasi-heterogeneous clusters
-qhet_p_01 = np.array([0.002332, 0.01642, 0.01691, 0.01749])
-qhet_multipliers = np.array([1., 1.5984, 2.1425, 2.663, 3.17])
+het_p01 = np.array([0.002332, 0.01642, 0.01691, 0.01749])
+het_multipliers = np.array([1., 1.5984, 2.1425, 2.663, 3.17])
 
 # Load parameters for heterogeneous clusters
-het_p_01 = np.array([0.001, 0.005, 0.009, 0.013])
-het_multipliers = np.array([1.974, 3.162, 4.245, 5.282, 6.292])
-
-# Load parameters for heterogeneous clusters
-hom_p_01 = het_p_01.mean() * np.ones(len(het_p_01))
+hom_p01 = 0.007 * np.ones(C)
 hom_multipliers = np.array([1.82, 2.909, 3.899, 4.847, 5.771])
+
+p01_25 = np.array([0.00385, 0.031, 0.032, 0.033])
 
 # Algorithm parameters
 dt_detection_thr = 0.95
-risk_step = 0.5
-risk_thr_vec = np.arange(0., 1. + risk_step, risk_step, dtype=float)
+ETA = 0.005
 
 # Schedulers
 # Schemes
-dist_schedulers = ['PPS', 'PPS-cluster', 'PPS-nodes', 'RR', 'random']
+dist_schedulers = ['PPS', 'CRA', 'MAF']
 
 def std_bar(total_iteration):
     return tqdm(total_iteration, file=sys.stdout, leave=False, ncols=60, ascii=True)
