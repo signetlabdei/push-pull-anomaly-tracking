0ee8954db46cc30f7c2d926bd852463a4fcfec70
lostinafro <saggese.fabio@gmail.com>
2025-08-07 12:01:07 +0200

    Added parallel simulation if --parallel flag is given. Refactored evry name for coherence with the paper

--- coexistence_manager_analysis.py
+++ coexistence_manager_analysis.py
@@ -1,17 +1,22 @@
+import os
+import time
+from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 import pandas as pd
-import os
+
 from coexistence_frame_analysis import run_episode
-from common import N, R, C, D, T, E, M, RNG, max_age, SIGMA, ETA, het_p01, het_multipliers, p01_25, p11, dt_detection_thr, std_bar, coexistence_folder
+import common as cmn
 
 if __name__ == '__main__':
+    # Parse arguments, if any
+    parallel, savedir, debug, overwrite = cmn.common_parser()
+    if savedir is not None:
+        coexistence_folder = savedir
+    else:
+        coexistence_folder = cmn.coexistence_folder
+
     # Simulation variables
     dec = 6
-    debug = False
-    overwrite = True
-
-    # Parameters
-    # aoii_thr = 3
     min_P = 5
     managers = np.array([1, 2])
     manager_names = ['RSM', 'SSM']
@@ -30,14 +35,11 @@ if __name__ == '__main__':
             if os.path.exists(filename) and not overwrite:
                 aoii = pd.read_csv(filename).iloc[:, 1:].to_numpy()
             else:
-                aoii = np.full((2, 6), np.nan)
+                aoii = np.full((2, len(column_titles)), np.nan)
 
             # Get load
             anomaly_rate = 0.03 if load == 'hom' else 0.035
-            p_01 = het_p01 * het_multipliers[2] if load == 'hom' else p01_25
-
-            # Test aoii threshold
-
+            p_01 = cmn.het_p01 * cmn.het_multipliers[2] if load == 'hom' else cmn.p01_25
 
             # Start iterations
             for m, manager in enumerate(managers):
@@ -46,29 +48,35 @@ if __name__ == '__main__':
 
                 # Check if data is there
                 if overwrite or np.all(np.isnan(aoii[m])):
-                    loca_aoii_hist = np.zeros(M + 1)
-                    dist_aoii_hist = np.zeros(M + 1)
-                    for ep in range(E):
-                        print(f'\tEpisode: {ep:02d}/{E - 1:02d}')
-                        loc_tmp, dist_tmp = run_episode(M, T, R,
-                                                        N, max_age, anomaly_rate, SIGMA, aoii_thr,
-                                                        C, D, p_01, p11, dt_detection_thr,
-                                                        manager, min_P, ETA,
-                                                        RNG, debug)
-                        loca_aoii_hist += loc_tmp[0] / E
-                        dist_aoii_hist += dist_tmp[0] / E
+                    args = (cmn.M, cmn.T, cmn.R, cmn.N, cmn.max_age, anomaly_rate, cmn.SIGMA, aoii_thr,
+                            cmn.C, cmn.D, p_01, cmn.p11, cmn.dt_realign_thr, manager, min_P, cmn.ETA, debug)
 
-                    # Local
-                    loca_aoii_cdf = np.cumsum(loca_aoii_hist)
-                    aoii[m, 0] = np.dot(loca_aoii_hist, np.arange(0, M + 1, 1))
-                    aoii[m, 1] = np.where(loca_aoii_cdf > 0.99)[0][0]
-                    aoii[m, 2] = np.where(loca_aoii_cdf > 0.999)[0][0]
+                    start_time = time.time()
+                    if parallel:
+                        with ProcessPoolExecutor() as executor:
+                            futures = [executor.submit(run_episode, ep, *args) for ep in range(cmn.E)]
+                            results = [f.result() for f in futures]
+                    else:
+                        results = []
+                        for ep in range(cmn.E):
+                            print(f'\tEpisode: {ep:02d}/{cmn.E - 1:02d}')
+                            results.append(run_episode(ep, *args))
 
-                    # Dist
-                    dist_aoii_cdf = np.cumsum(dist_aoii_hist)
-                    aoii[m, 3] = np.dot(dist_aoii_hist, np.arange(0, M + 1, 1))
-                    aoii[m, 4] = np.where(dist_aoii_cdf > 0.99)[0][0]
-                    aoii[m, 5] = np.where(dist_aoii_cdf > 0.999)[0][0]
+                    # Separate and average the results
+                    anom_aoii_hist = np.mean(np.array([res[0][0] for res in results]), axis=0)
+                    drift_aoii_hist = np.mean(np.array([res[1][0] for res in results]), axis=0)
+
+                    # Anomalies
+                    anom_aoii_cdf = np.cumsum(anom_aoii_hist)
+                    aoii[m, 0] = np.dot(anom_aoii_hist, np.arange(0, cmn.M + 1, 1))
+                    aoii[m, 1] = np.where(anom_aoii_cdf > 0.99)[0][0]
+                    aoii[m, 2] = np.where(anom_aoii_cdf > 0.999)[0][0]
+
+                    # DT drifts
+                    drift_aoii_cdf = np.cumsum(drift_aoii_hist)
+                    aoii[m, 3] = np.dot(drift_aoii_hist, np.arange(0, cmn.M + 1, 1))
+                    aoii[m, 4] = np.where(drift_aoii_cdf > 0.99)[0][0]
+                    aoii[m, 5] = np.where(drift_aoii_cdf > 0.999)[0][0]
 
                     # Generate data frame and save it (redundant but to avoid to lose data for any reason)
                     df = pd.DataFrame(aoii.round(dec), columns=column_titles)
