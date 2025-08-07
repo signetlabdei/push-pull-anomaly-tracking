3e2b2da27a1c5ecc94950fda2cbe6b297c9055ba
lostinafro <saggese.fabio@gmail.com>
2025-08-07 15:50:06 +0200

    Added parallel simulation if --parallel flag is given. Refactored every name for coherence with the paper

--- push_cdf_analysis.py
+++ push_cdf_analysis.py
@@ -1,79 +1,73 @@
+import os
+import time
+from concurrent.futures import ProcessPoolExecutor
 import numpy as np
-from local_scheduler import LocalAnomalyScheduler
+import pandas as pd
 
+from push_frame_analysis import run_episode
+import common as cmn
 
-def run_episode(M, P, T, nodes, max_age, local_anomaly_rate, p_c, mode, debug_mode):
-    # Instantiate scheduler
-    local_sched = LocalAnomalyScheduler(nodes, max_age, local_anomaly_rate, mode, debug_mode)
-
-    local_state = np.zeros(nodes)
-    aoii = np.zeros((T, nodes))
-
-    for t in range(T):
-        ### ANOMALY GENERATION ###
-        local_state = np.minimum(np.ones(nodes), local_state + np.asarray(np.random.rand(nodes) < local_anomaly_rate))
-
-        ### COMPUTE AOII ###
-        if t > 0:
-            aoii[t, :] = aoii[t - 1, :] + local_state
-        else:
-            aoii[t, :] = local_state
-
-        ### UPDATE SCHEDULER PRIORS ###
-        local_sched.update_prior()
-
-        ### PUSH-BASED SUBFRAME ###
-        # Get local anomaly threshold
-        threshold = local_sched.schedule(P, p_c, [])
-        # Select random slots for active nodes
-        choices = np.random.randint(1, P + 1, nodes) * np.asarray(aoii[t, :] > threshold)
-        outcome = np.zeros(P, dtype=int)
-        for p in range(1, P + 1):
-            chosen = np.where(choices == p)[0]
-            if chosen.size != 0:
-                if chosen.size == 1:
-                    outcome[p - 1] = chosen[0] + 1
-                    local_state[chosen[0]] = 0
-                    aoii[t, chosen[0]] = 0
-                else:
-                    outcome[p - 1] = -1
-
-        ### POST-FRAME UPDATE ###
-        # Local anomaly belief update
-        local_sched.update_psi(threshold, outcome)
-
-        ### LOGGING ###
-        if np.mod(t,1000) == 0:
-            print('Step:', t)
-
-    # Plotting local anomaly AoII
-    aoii_tot = np.reshape(aoii, T * nodes)
-    return np.histogram(aoii_tot, bins=M + 1, range=(-0.5, M + 0.5), density=True)
 
 if __name__ == "__main__":
-    # Main system parameters
-    nodes = 100
-    max_age = 100
-    M = 100     # S = 20
+    # Parse arguments, if any
+    parallel, savedir, debug, overwrite = cmn.common_parser()
+    if savedir is not None:
+        push_folder = savedir
+    else:
+        push_folder = cmn.push_folder
+    # Simulation variables
+    dec = 6
+    pps_scheduler_mode = 1
     P = 10
-    T = int(1e4)
-    episodes = 10
-    mode = 1
-    debug_mode = False
-
     # Anomaly and algorithm parameters
     rates = np.arange(0.01, 0.051, 0.01)
-    p_c = 0.2
 
-    aoii_hist = np.zeros((len(rates) + 1, M + 1))
+    # Check if files exist and load it if there
+    prefix = 'push'
+    filename_cdf = os.path.join(push_folder, prefix + '_cdf.csv')
 
-    for r in range(len(rates)):
-        rate = rates[r]
-        print('Rate: ', rate)
-        for ep in range(episodes):
-            print('Episode: ', ep)
-            aoii_hist[r + 1, :] += np.cumsum(run_episode(M, P, T, nodes, max_age, rate, p_c, mode, debug_mode)[0]) / episodes
-    aoii_hist[0, :] = np.arange(0, M + 1, 1)
+    if os.path.exists(filename_cdf) and not overwrite:
+        cdf = pd.read_csv(filename_cdf).iloc[:, 1:].to_numpy().T
+    else:
+        cdf = np.full((len(rates), cmn.M + 1), np.nan)
 
-    np.savetxt("push_cdf.csv", np.transpose(aoii_hist), delimiter=",")
+
+    for r, rate in enumerate(rates):
+        # Logging #
+        print(f"Rate={rate * 100:.1f}. Status:")
+
+        # Check if data is there
+        if overwrite or np.any(np.isnan(cdf[r])):
+            args = (3, cmn.M, P, cmn.T, cmn.N, cmn.max_age,
+                    rate, cmn.SIGMA, pps_scheduler_mode, debug)
+
+            start_time = time.time()
+            if parallel:
+                with ProcessPoolExecutor() as executor:
+                    futures = [executor.submit(run_episode, ep, *args) for ep in range(cmn.E)]
+                    results = [f.result() for f in futures]
+            else:
+                results = []
+                for ep in range(cmn.E):
+                    print(f'\tEpisode: {ep:02d}/{cmn.E - 1:02d}')
+                    results.append(run_episode(ep, *args))
+
+            # Average the results
+            anomaly_aoii_hist = np.mean(np.array([res[0] for res in results]), axis=0)
+
+            # Divide data
+            cdf[r] = np.cumsum(anomaly_aoii_hist)
+
+            # Generate data frame and save it (redundant but to avoid to lose data for any reason)
+            df = pd.DataFrame(cdf.T.round(dec), columns=['p1', 'p2', 'p3', 'p4', 'p5'])
+            df.insert(0, 'Theta', np.arange(cmn.M + 1))
+            df.to_csv(filename_cdf, index=False)
+
+            # Print time
+            elapsed = time.time() - start_time
+            print(f"\t...done in {elapsed:.3f} seconds")
+
+        else:
+            print("\t...already done!")
+            continue
 
