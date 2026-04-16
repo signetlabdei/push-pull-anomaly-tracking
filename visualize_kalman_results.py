301611895c7ee9bc0aa6d988a9b1d4d73d7a05f0
lostinafro <saggese.fabio@gmail.com>
2026-04-16 14:55:54 +0200

    New results visualization

--- visualize_kalman_results.py
+++ visualize_kalman_results.py
@@ -7,7 +7,7 @@ cmn.latex_look(plt)
 
 # Pull folder
 folder = cmn.pull_folder
-prefixes = ["pull_process_kalman", "pull_frame_kalman"]
+prefixes = ["pull_process_kalman", "pull_frame_kalman", "pull_noise_kalman", "pull_noise_kalman_50"]
 suffixes = ["_avg", "_99", "_999"]
 suf_ylabels = ["avg. MSE", "MSE 99-th", "MSE 999-th"]
 num_results = len(suffixes)
@@ -20,7 +20,6 @@ for s, suffix in enumerate(suffixes):
     results = pd.read_csv(filename).iloc[:, :].to_numpy().T
 
     axes[s].plot(results[0], results[1:].T, label=cmn.pull_scheduler_names)
-    # axes[i].set_title(cmn.results_label[i])
     axes[s].set_ylabel(suf_ylabels[s])
     axes[s].set_xlabel(r"$\sigma_w$")
 
@@ -50,3 +49,22 @@ for i in range(num_results):
     axes[i].legend()
 fig.tight_layout()
 fig.show()
+
+for prefix in prefixes[2:]:
+    # Noise
+    ## Process
+    fig, axes = plt.subplots(ncols=1, nrows=num_results, figsize=(4, 15))
+    for s, suffix in enumerate(suffixes):
+        filename = os.path.join(folder, prefix + suffix + '.csv')
+        # read data
+        results = pd.read_csv(filename).iloc[:, :].to_numpy().T
+
+        axes[s].plot(results[0], results[1:].T, label=cmn.pull_scheduler_names)
+        axes[s].set_ylabel(suf_ylabels[s])
+        axes[s].set_xlabel(r"$\hat{\sigma}_w$")
+
+    for i in range(num_results):
+        axes[i].grid(True)
+        axes[i].legend()
+    fig.tight_layout()
+    fig.show()
